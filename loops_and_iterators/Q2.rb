89b0b2f0cc37b23195f1f1eb2e83c94e3404d93d
Jiang Haohao <jianghaohao@huantengsmart.com>
2016-11-06 16:06:43 +0800

    fix after reading solution

--- loops_and_iterators/Q2.rb
+++ loops_and_iterators/Q2.rb
@@ -3,8 +3,12 @@ begin
   answer = gets.chomp
 end while answer != 'STOP'
 
+puts "# finish first version"
+
 answer = ''
 while answer != 'STOP'
   puts "Type 'STOP' to stop."
   answer = gets.chomp
 end
+
+puts "# finish second version"
--- loops_and_iterators/Q4.rb
+++ loops_and_iterators/Q4.rb
@@ -1,6 +1,7 @@
+# fix after reading solution
 def count_down(n)
   puts n
-  count_down(n-1) unless n == 0
+  count_down(n-1) unless n <= 0
 end
 
 count_down(8)
