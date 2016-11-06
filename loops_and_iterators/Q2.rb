bd51c2589b09d840172fc7b9d8a205528def03f4
Jiang Haohao <jianghaohao@huantengsmart.com>
2016-11-06 16:02:45 +0800

    finish chapter Loops & Iterators

---
+++ loops_and_iterators/Q2.rb
@@ -0,0 +1,10 @@
+begin
+  puts "Type 'STOP' to stop."
+  answer = gets.chomp
+end while answer != 'STOP'
+
+answer = ''
+while answer != 'STOP'
+  puts "Type 'STOP' to stop."
+  answer = gets.chomp
+end
---
+++ loops_and_iterators/Q4.rb
@@ -0,0 +1,8 @@
+def count_down(n)
+  puts n
+  count_down(n-1) unless n == 0
+end
+
+count_down(8)
+
+count_down(-10)
